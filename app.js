2be3baf8baff7ff0fad6120746996fdca522b5a0
Pijush Ghorai <pghorai098@gmail.com>
2024-06-18 01:11:32 +0530

    blog post page

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ document.getElementById('menu-btn').addEventListener('click', function() {
 document.getElementById('menu-close').addEventListener('click', function() {
     document.querySelector('nav .navigation ul').classList.remove('active');
 });
+
+let apiKey = "657303909fe54f2196c88bd631592398"
+let url = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${apiKey}`
+fetch(url).then((res) => {
+    return res.json()
+}).then((data) => {
+    console.log(data);
+})
+
