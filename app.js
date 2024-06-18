8d2482afa42ec692441c052c1c884cd996366573
Pijush Ghorai <pghorai098@gmail.com>
2024-06-19 01:56:55 +0530

    responsive courses page

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-document.getElementById('menu-btn').addEventListener('click', function() {
-    document.querySelector('nav .navigation ul').classList.add('active');
+document.getElementById("menu-btn").addEventListener("click", function () {
+  document.querySelector("nav .navigation ul").classList.add("active");
 });
 
-document.getElementById('menu-close').addEventListener('click', function() {
-    document.querySelector('nav .navigation ul').classList.remove('active');
+document.getElementById("menu-close").addEventListener("click", function () {
+  document.querySelector("nav .navigation ul").classList.remove("active");
 });
 
-let apiKey = "657303909fe54f2196c88bd631592398"
-let url = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${apiKey}`
-fetch(url).then((res) => {
-    return res.json()
-}).then((data) => {
+let apiKey = "657303909fe54f2196c88bd631592398";
+let url = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${apiKey}`;
+fetch(url)
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
     console.log(data);
-})
+  });
 
+document.querySelector(".course-card").addEventListener("click", function () {
+  window.location.href = "course-details.html";
+});
