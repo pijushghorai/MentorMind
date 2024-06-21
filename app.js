035802533b432427bb82faf58fe3439d43c41db0
Pijush Ghorai <pghorai098@gmail.com>
2024-06-21 21:05:05 +0530

    adding faq section

--- app.js
+++ app.js
@@ -6,6 +6,22 @@ document.getElementById("menu-close").addEventListener("click", function () {
   document.querySelector("nav .navigation ul").classList.remove("active");
 });
 
+
+// accordian
+const accordian = document.querySelectorAll(".accordian");
+
+accordian.forEach(acco => {
+  const icon = acco.querySelector(".fa-caret-down");
+  const answer = acco.querySelector(".answer");
+
+  acco.addEventListener("click", function() {
+    icon.classList.toggle('iconactive')
+    answer.classList.toggle('active')
+  })
+})
+
+
+
 let apiKey = "657303909fe54f2196c88bd631592398";
 let url = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${apiKey}`;
 fetch(url)
